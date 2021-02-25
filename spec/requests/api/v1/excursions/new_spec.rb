0895f13f801cb36ae30bd9b0dda7951fd5a10b97
Yesi Meza <yesi.meza10@gmail.com>
2021-02-25 15:49:04 -0700

    removed a pry and fixed testing expeectation to be a key
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- spec/requests/api/v1/excursions/new_spec.rb
+++ spec/requests/api/v1/excursions/new_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe "excursions" do
   it "can create an excursions " do
-    require 'pry'; binding.pry
     user = create(:user)
     excursion_params = {
                       title: Faker::Coffee.blend_name, 
@@ -13,15 +12,15 @@ describe "excursions" do
 
     headers = {"CONTENT_TYPE" => "application/json"}
 
-    post '/api/v1/excursions', headers: headers, params: JSON.generate(excursion_params)
+    post "/api/v1/users/#{user.id}/excursions", headers: headers, params: JSON.generate(excursion_params)
 
     created_excursion = Excursion.last
 
     expect(response).to be_successful
     expect(response).to have_http_status(:created)
-    expect(created_excursion.title).to eq(excursion_params.title)
-    expect(created_excursion.description).to eq(excursion_params.description)
-    expect(created_excursion.location).to eq(excursion_params.location)
-    expect(created_excursion.user_id).to eq(excursion_params.uder_id)
+    expect(created_excursion.title).to eq(excursion_params[:title])
+    expect(created_excursion.description).to eq(excursion_params[:description])
+    expect(created_excursion.location).to eq(excursion_params[:location])
+    expect(created_excursion.user_id).to eq(excursion_params[:user_id])
     end 
   end
