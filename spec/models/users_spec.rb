fbbc986b1a7d7924393ca3de0cfaafb2228319f3
Isabelle Villasenor <villasenori@outlook.com>
2021-02-24 17:51:21 -0700

    Add FactoryBot Instantion Specs
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -4,8 +4,17 @@ RSpec.describe User do
   describe 'validations' do
     it { should validate_presence_of :id }
   end
+
   describe 'relationships' do
     it { should have_many :favorites }
     it { should have_many :excursions }
+  end
+
+  describe 'instantiation' do
+    it 'exists with attributes' do
+      user1 = create(:user, id: 1)
+
+      expect(user1).to be_instance_of User
+    end
   end
 end
