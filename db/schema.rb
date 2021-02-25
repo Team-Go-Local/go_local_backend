e7ed1fbabb637fe71e34d2580218bc4fcef970a4
Sam Yeo <yeosamuel95@gmail.com>
2021-02-24 18:19:17 -0700

    Correct DB foreign_keys instead of using index
    Co-authored-by: Isabelle Villasenor <45639675+isabellevillasenor@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_001324) do
+ActiveRecord::Schema.define(version: 2021_02_25_011705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,9 @@ ActiveRecord::Schema.define(version: 2021_02_25_001324) do
   create_table "users", force: :cascade do |t|
   end
 
+  add_foreign_key "excursions", "users"
   add_foreign_key "favorites", "excursions"
+  add_foreign_key "favorites", "users"
   add_foreign_key "taggings", "excursions"
   add_foreign_key "taggings", "tags"
 end
