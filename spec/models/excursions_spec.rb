09a696ad97bf28d04b41e8bb5cfcfb70b4a9c7f9
Isabelle Villasenor <villasenori@outlook.com>
2021-02-24 17:31:14 -0700

    Add Factory Initialization Spec to Excursion Model Spec
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- spec/models/excursions_spec.rb
+++ spec/models/excursions_spec.rb
@@ -6,10 +6,21 @@ RSpec.describe Excursion do
     it { should validate_presence_of :description }
     it { should validate_presence_of :location }
   end
+
   describe 'relationships' do
     it { should have_many :taggings }
     it { should have_many(:tags).through(:taggings) }
     it { should have_many :favorites }
     it { should belong_to :user }
   end
+
+  describe 'instantiation' do
+    it 'exists with attributes' do
+      user1 = create(:user, id: 1)
+      ex1 = create(:excursion, author_id: user1.id)
+
+      expect(ex1).to be_instance_of Excursion
+    end
+  end
+
 end
