6ee6b62f528de2edc604a9ca673c7b6b52b9d782
Yesi Meza <yesi.meza10@gmail.com>
2021-02-25 15:50:15 -0700

    created route for create excursion
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/users/:id/excursions', to: 'excursions#create'
       post '/users/:id', to: 'users#create'
     end
   end
