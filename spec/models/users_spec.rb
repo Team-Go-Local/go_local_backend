aa0b29e8551e7ae6191497248605fa75529d5240
Yesi Meza <yesi.meza10@gmail.com>
2021-02-25 15:54:22 -0700

    removed comments and not necessary model validation test
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  describe 'validations' do
-    it { should validate_presence_of :id }
-  end
-
   describe 'relationships' do
     it { should have_many :favorites }
     it { should have_many :excursions }
