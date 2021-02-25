4067ea68051dbd2dbdac8c9d397719a9255203b0
Isabelle Villasenor <villasenori@outlook.com>
2021-02-24 17:29:23 -0700

    Add Primary Key to Users Table
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_024043) do
+ActiveRecord::Schema.define(version: 2021_02_25_001324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,9 @@ ActiveRecord::Schema.define(version: 2021_02_24_024043) do
     t.string "title"
     t.string "description"
     t.string "location"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_excursions_on_user_id"
   end
 
@@ -43,8 +43,7 @@ ActiveRecord::Schema.define(version: 2021_02_24_024043) do
     t.string "name"
   end
 
-  create_table "users", id: false, force: :cascade do |t|
-    t.integer "id"
+  create_table "users", force: :cascade do |t|
   end
 
   add_foreign_key "favorites", "excursions"
