6a9e5212aa7ff8bf27194def9857177c0840227b
Yesi Meza <yesi.meza10@gmail.com>
2021-02-25 20:29:41 -0700

    cleaned up the routes and added a namespace, edited the contoller name
    Co-authored-by: Cydnee Owens <67389821+cowens87@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post '/users/:id/excursions', to: 'excursions#create'
+
       post '/users/:id', to: 'users#create'
+
+      namespace :users do  
+        post '/:id/excursions', to: 'excursions#create'
+        patch '/:id/excursions/:id', to: 'excursions#update'
+      end 
     end
   end
 end
