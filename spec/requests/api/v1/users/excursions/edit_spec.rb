058777502764303c6b87d9f0bef1e0caede3a172
Cydnee Owens <cks410@yahoo.com>
2021-02-27 06:21:14 -0800

    remove issues from edit spec per rubocop

--- spec/requests/api/v1/users/excursions/edit_spec.rb
+++ spec/requests/api/v1/users/excursions/edit_spec.rb
@@ -1,14 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe "excursions" do
-  it "can update an excursions " do
+describe 'excursions' do
+  it 'can update an excursions' do
     user = User.create(id: 1)
 
-    excursion = create(:excursion, user_id: user.id  )
+    excursion = create(:excursion, user_id: user.id)
     previous_name = Excursion.last.title
-    excursion_params = {title: "Excursion303"}
-  
-    headers = {"CONTENT_TYPE" => "application/json"}
+    excursion_params = { title: 'Excursion303' }
+    headers = { 'CONTENT_TYPE' => 'application/json' }
 
     patch "/api/v1/users/#{user.id}/excursions/#{excursion.id}", headers: headers, params: JSON.generate(excursion_params)
 
@@ -16,5 +17,5 @@ describe "excursions" do
     expect(response).to be_successful
     expect(updated_excursion.title).to_not eq(previous_name)
     expect(updated_excursion.title).to eq(excursion_params[:title])
-    end 
   end
+end
