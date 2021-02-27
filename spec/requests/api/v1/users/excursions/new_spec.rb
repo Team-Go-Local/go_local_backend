dd4e64e72ce4a7ac6961a876ba78fcf2b607bb08
Cydnee Owens <cks410@yahoo.com>
2021-02-27 06:16:24 -0800

    remove issues from new spec per rubocop

--- spec/requests/api/v1/users/excursions/new_spec.rb
+++ spec/requests/api/v1/users/excursions/new_spec.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe "excursions" do
-  it "can create an excursions " do
+describe 'excursions' do
+  it 'can create an excursions ' do
     user = User.create(id: 1)
     excursion_params = {
-                      title: Faker::Coffee.blend_name, 
-                      description: Faker::Coffee.notes,
-                      location: Faker::Address.city,
-                      user_id: user.id 
-                       }
+                          title: Faker::Coffee.blend_name,
+                          description: Faker::Coffee.notes,
+                          location: Faker::Address.city,
+                          user_id: user.id
+                        }
 
-    headers = {"CONTENT_TYPE" => "application/json"}
+    headers = { 'CONTENT_TYPE' => 'application/json' }
 
     post "/api/v1/users/#{user.id}/excursions", headers: headers, params: JSON.generate(excursion_params)
 
@@ -22,5 +24,5 @@ describe "excursions" do
     expect(created_excursion.description).to eq(excursion_params[:description])
     expect(created_excursion.location).to eq(excursion_params[:location])
     expect(created_excursion.user_id).to eq(excursion_params[:user_id])
-    end 
   end
+end
