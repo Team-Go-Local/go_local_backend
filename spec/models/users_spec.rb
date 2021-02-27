19d778bcfe8ce08c88f741707d38d68a3fb8173a
Cydnee Owens <cks410@yahoo.com>
2021-02-27 06:42:33 -0800

    remove issues from users spec per rubocop

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User do
   describe 'validations' do
     it { should validate_presence_of :id }
   end
-  
+
   describe 'relationships' do
     it { should have_many :favorites }
     it { should have_many :excursions }
