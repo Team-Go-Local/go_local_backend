a0f9ab2fcf3466ea418447fb76c3a683e35dcd7b
Cydnee Owens <cks410@yahoo.com>
2021-02-27 06:39:29 -0800

    remove issues from index spec per rubocop

--- spec/requests/api/v1/excursions/index_spec.rb
+++ spec/requests/api/v1/excursions/index_spec.rb
@@ -1,20 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe "Excursions Index Endpoint" do
+describe 'Excursions Index Endpoint' do
   describe 'GET /excursions' do
-    it "can fetch all excursion" do
+    it 'can fetch all excursion' do
       user = User.create(id: 1).id
-      excursion1 = create(:excursion, user_id: user)
-      excursion2 = create(:excursion, user_id: user)
-      excursion3 = create(:excursion, user_id: user)
-      excursion4 = create(:excursion, user_id: user)
-      excursion5 = create(:excursion, user_id: user)
-      excursion6 = create(:excursion, user_id: user)
+      create_list(:excursion, 6, user_id: user)
 
       get '/api/v1/excursions'
 
       expect(response).to be_successful
-    
       excursions = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(excursions.count).to eq(6)
