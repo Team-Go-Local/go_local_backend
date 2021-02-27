529928312057973c023d3bb2505de1f55cb56ece
Cydnee Owens <cks410@yahoo.com>
2021-02-27 06:37:18 -0800

    remove issues from show spec per rubocop

--- spec/requests/api/v1/excursions/show_spec.rb
+++ spec/requests/api/v1/excursions/show_spec.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-describe "Excursions Show Endpoint" do
+describe 'Excursions Show Endpoint' do
   describe 'GET /excursions/:id' do
-    it "can fetch one excursion" do
+    it 'can fetch one excursion' do
       user = User.create(id: 1).id
       excursion1 = create(:excursion, user_id: user)
 
       get "/api/v1/excursions/#{excursion1.id}"
-
       expect(response).to be_successful
-    
       excursion = JSON.parse(response.body, symbolize_names: true)[:data]
 
       check_hash_structure(excursion, :type, String)
