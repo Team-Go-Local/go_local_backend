b9c34811e24d782ce153ab6a486e36a081ed72cb
Sam Yeo <yeosamuel95@gmail.com>
2021-02-27 21:23:28 -0700

    Create place_details class method
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- app/facades/place_search_facade.rb
+++ app/facades/place_search_facade.rb
@@ -1,6 +1,11 @@
 class PlaceSearchFacade
   class << self
 
+    def place_details(title, location)
+      query_details = format_details(title, location)
+      PlaceSearchService.get_location_details(query_details)
+    end
+
     def format_details(title, location)
       ((title +"-"+ location).delete(",")).gsub(" ", "-")
     end
