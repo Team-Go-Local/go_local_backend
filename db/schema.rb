a6122aefc70214865314a6da3278b13621c18e1f
Sam Yeo <yeosamuel95@gmail.com>
2021-02-27 21:53:04 -0700

    Add place_id column to Excursions
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_25_011705) do
+ActiveRecord::Schema.define(version: 2021_02_28_045135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_02_25_011705) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "place_id"
     t.index ["user_id"], name: "index_excursions_on_user_id"
   end
 
