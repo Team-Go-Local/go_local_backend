13f5afb7b4342ee9c7722770ae0cd90a8d9af6ba
Sam Yeo <yeosamuel95@gmail.com>
2021-02-27 21:44:49 -0700

    Refactor to call on facade and service
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- app/controllers/api/v1/place_search_controller.rb
+++ app/controllers/api/v1/place_search_controller.rb
@@ -1,11 +1,5 @@
 class Api::V1::PlaceSearchController < ApplicationController
   def receive_fe_request
-    place_details = (params[:title] +"-"+ params[:location].delete(",")).gsub(" ", "-")
-    conn = Faraday.new(url: "https://go-local-maps-api.herokuapp.com")
-    response = conn.get("/api/v1/place_search") do |request|
-      request.params['location'] = place_details
-    end
-
-    render json: response.body
+    render json: PlaceSearchFacade.place_details(params[:title], params[:location])
   end
 end
