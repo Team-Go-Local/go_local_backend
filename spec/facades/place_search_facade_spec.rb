7fd85131886ae6fa5a32fccca51c090fd16159e5
Sam Yeo <yeosamuel95@gmail.com>
2021-02-27 21:21:42 -0700

    Create PlaceSearchFacade test for method place_details
    Co-authored-by: Saundra Catalina <68261312+saundracatalina@users.noreply.github.com>

--- spec/facades/place_search_facade_spec.rb
+++ spec/facades/place_search_facade_spec.rb
@@ -9,4 +9,15 @@ describe 'PlaceSearchFacade' do
 
     expect(PlaceSearchFacade.format_details(title, location)).to eq(result)
   end
+
+  it '.place_details' do
+    fixture_json = File.read('spec/fixtures/place_search_result.json')
+    be_search_details = "casa-bonita-Denver-CO"
+    stub_request(:get, "https://go-local-maps-api.herokuapp.com/api/v1/place_search?location=#{be_search_details}").to_return(status: 200, body: fixture_json)
+    
+    title = "casa bonita"
+    location = "Denver, CO"
+
+    expect(PlaceSearchFacade.place_details(title, location).body).to eq(fixture_json)
+  end
 end
