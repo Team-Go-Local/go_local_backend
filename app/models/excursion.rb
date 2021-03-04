1013be228d2e528803b72e56a437b71ee110fa47
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-03 19:19:59 -0700

    add cities method to excursions

--- app/models/excursion.rb
+++ app/models/excursion.rb
@@ -7,4 +7,8 @@ class Excursion < ApplicationRecord
   belongs_to :user
 
   alias_attribute :author_id, :user_id
+
+  def self.cities
+    distinct.pluck(:nearest_city)
+  end
 end
--- spec/models/excursions_spec.rb
+++ spec/models/excursions_spec.rb
@@ -24,4 +24,14 @@ RSpec.describe Excursion do
       expect(ex1).to be_instance_of Excursion
     end
   end
+
+  it 'gets a list of its cities' do
+    create_list(:excursion, 2, nearest_city: 'Chicago, IL')
+    create(:excursion, nearest_city: 'Philadelphia, PA')
+    create_list(:excursion, 2, nearest_city: 'San Antonio, TX')
+    create(:excursion, nearest_city: 'Austin, TX')
+
+    cities = ['Chicago, IL', 'Philadelphia, PA', 'San Antonio, TX', 'Austin, TX']
+    expect(Excursion.cities).to match_array(cities)
+  end
 end
