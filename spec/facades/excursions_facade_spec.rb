1344b6547d0aec0c5599f717ef9e3171b3dc40f7
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-03 18:59:06 -0700

    add excursions facade with nearest city method

--- spec/facades/excursions_facade_spec.rb
+++ spec/facades/excursions_facade_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe ExcursionsFacade do
   it 'adds the nearest city upon creation' do
-    city = ExcursionsFacade.nearest_city('(43.606577, -72.97946999999999)')
-    expect(city).to eq('New Orleans, LA')
+    VCR.use_cassette('new_orleans') do
+      city = ExcursionsFacade.nearest_city('(29.9754713, -90.0851898)')
+      expect(city).to eq('New Orleans, LA')
+    end
   end
 end
