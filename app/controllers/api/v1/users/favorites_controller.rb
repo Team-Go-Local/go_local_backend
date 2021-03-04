8eb667a2d5a684c6093619bb71b74a643a405463
Sam Yeo <yeosamuel95@gmail.com>
2021-03-04 12:54:55 -0700

    Move add_favorite method to namespaced controller

--- app/controllers/api/v1/users/favorites_controller.rb
+++ app/controllers/api/v1/users/favorites_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::Users::FavoritesController < ApplicationController
   def index
-
     favorites = User.find(params[:id]).saved_excursions
     options = {}
     options[:meta] = { cities: favorites.cities }
     render json: ExcursionSerializer.new(favorites, options)
+  end
+  def add_favorite
+    render json: FavoriteSerializer.new(Favorite.create(favorite_params))
   end
 end
