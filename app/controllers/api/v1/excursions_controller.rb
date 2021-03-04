43564e64b523bd1e4262981e78eeb5a42f536c5b
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-03 22:25:20 -0700

    correct rubocop infractions

--- app/controllers/api/v1/excursions_controller.rb
+++ app/controllers/api/v1/excursions_controller.rb
@@ -1,21 +1,17 @@
 class Api::V1::ExcursionsController < ApplicationController
   def index
     excursions = if params[:city]
-                    Excursion.where(nearest_city: params[:city])
-                  else
-                    Excursion.all
-                  end
+                   Excursion.where(nearest_city: params[:city])
+                 else
+                   Excursion.all
+                 end
     options = {}
-    options[:meta] = { cities: excursions.cities}
+    options[:meta] = { cities: excursions.cities }
     render json: ExcursionSerializer.new(excursions, options)
   end
 
   def show
     excursion = Excursion.find(params[:id])
-    begin
-      render json: ExcursionSerializer.new(excursion)
-    rescue
-      render json: {errors: user.errors.full_messages.uniq.to_sentence}, status: 404
-    end
+    render json: ExcursionSerializer.new(excursion)
   end
 end
--- app/controllers/api/v1/users/favorites_controller.rb
+++ app/controllers/api/v1/users/favorites_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::Users::FavoritesController < ApplicationController
   def index
     favorites = User.find(params[:id]).saved_excursions
     options = {}
-    options[:meta] = { cities: favorites.cities}
+    options[:meta] = { cities: favorites.cities }
     render json: ExcursionSerializer.new(favorites, options)
   end
 end
